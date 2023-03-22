ba45cd70c94282f0233a774d07797637e6aae6b8
Ethan Dinh <107064019+DnguyenE@users.noreply.github.com>
2023-03-22 00:25:18 -0400

    Added simple vector template with output

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
 
 //int argc ==
 //char argv[] ==
-
+using namespace std;
 int main(int argc, char* argv[]){
-	return 0;
+
+	vector<vector<int> > nums {{1,2,3},{4,5,6},{7,8,9}};
+
+	for (int i = 0; i < nums.size(); i++) {
+		for (int j = 0; j < nums[i].size();j++){
+			cout << nums[i][j] << " ";
+		}
+	cout<<endl;
+	}
 }
