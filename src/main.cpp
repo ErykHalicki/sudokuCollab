9e596d4bf40017c9777dfb93fc765eacf7ea3ccb
Ethan Dinh <107064019+DnguyenE@users.noreply.github.com>
2023-03-22 22:57:58 -0400

    Added Grid in Terminal Sudoku

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,34 @@
 //int argc ==
 //char argv[] ==
 using namespace std;
+
 int main(int argc, char* argv[]){
 
-	vector<vector<int> > nums {{1,2,3},{4,5,6},{7,8,9}};
-
-	for (int i = 0; i < nums.size(); i++) {
-		for (int j = 0; j < nums[i].size();j++){
-			cout << nums[i][j] << " ";
+	vector<vector<int> > nums {{0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0,0}};
+	//loop for printing out grid layout 
+	for (int row = 0; row < nums.size(); row++) {
+		//using a while loop for horizontal line
+		int counter = 11;
+		if (row == 3 || row == 6){
+			while (counter > 0){
+				cout << "- ";
+				counter--;
+			}
+			cout<<endl;
+		}
+		for (int col = 0; col < nums[row].size();col++){
+			cout << nums[row][col] << " ";
+			//printing out veritical lines
+			if (col == 2 || col == 5) //remember that col starts at 0;
+				cout << "| ";
 		}
 	cout<<endl;
 	}
